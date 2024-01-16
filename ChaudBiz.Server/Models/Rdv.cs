add47f8d76003f32d773da8b9f5e8ede5763048f
Dupouy Lucie <luciedupouy@ensc.fr>
2024-01-16 16:29:46 +0100

    Rdv

--- ChaudBiz.Server/Models/Rdv.cs
+++ ChaudBiz.Server/Models/Rdv.cs
@@ -3,4 +3,6 @@ public class Rdv {
     public DateTime DateRdv {get;set;}
     public string Lieu {get;set;}
     public string Description {get;set;}
+    public Client Client {get;set;}
+    public Administrateur Administrateur {get;set;}
 }
