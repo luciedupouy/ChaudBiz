e09851852e64c8a21450be3a7bb1af558ad2b7c4
Dupouy Lucie <luciedupouy@ensc.fr>
2024-01-16 14:36:10 +0100

    Utilisateur

---
+++ ChaudBiz.Server/Models/Utilisateur.cs
@@ -0,0 +1,7 @@
+public abstract class Utilisateur {
+    public int Id {get;set;}
+    public string Nom {get; set;}
+    public string Prenom {get;set;}
+    public string Mail {get;set;}
+    public string Mdp {get;set;}
+}
